35a417bb57800576caf567bb0a9a9608173b92fb
yitzhak Matias <matias_6@hotmail.com>
2018-07-21 20:02:37 -0400

    Add project files.

---
+++ BookCatTest/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BookCatTest.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BookCatTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BookCatTest.WebApiApplication" Language="C#" %>
